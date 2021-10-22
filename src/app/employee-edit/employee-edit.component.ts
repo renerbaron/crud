8efe9ec42b7408336bac1cb244b4757d53bc6602
Batista <jose.matos.batista@accenture.com>
2021-10-21 22:20:34 -0300

    controler com metodos para consumo de serviços de atualizaçao

--- src/app/employee-edit/employee-edit.component.ts
+++ src/app/employee-edit/employee-edit.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiEmployeeService } from './../services/api-employee.service';
 
 @Component({
-  selector: 'app-employee-edit',
+  selector: 'app-employee-details',
   templateUrl: './employee-edit.component.html',
   styleUrls: ['./employee-edit.component.css']
 })
+
 export class EmployeeEditComponent implements OnInit {
+  id = this.actRoute.snapshot.params['id'];
+  employeeData: any = {};
 
-  constructor() { }
+  constructor(
+    public apiREST: ApiEmployeeService,
+    public actRoute: ActivatedRoute,
+    public router: Router
+  ) {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.apiREST.getEmployee(this.id).subscribe((data: {}) => {
+      this.employeeData = data;
+    })
+  }
+
+  // Update employee data
+  updateEmployee() {
+    if(window.confirm('Are you sure, you want to update?')){
+      this.apiREST.updateEmployee(this.id, this.employeeData).subscribe(data => {
+        this.router.navigate(['/employees-list'])
+      })
+    }
   }
 
 }
