65465a48499c94e2bf2182c16131baf1fe7a3b2a
Caitano <josiasmartinscaitano@gmail.com>
2021-10-21 22:16:37 -0300

    adicionado o componete de lista e seus métodos

--- src/app/employees-list/employees-list.component.ts
+++ src/app/employees-list/employees-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from "../shared/rest-api.service";
 
 @Component({
   selector: 'app-employees-list',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeesListComponent implements OnInit {
 
-  constructor() { }
+  Employee: any = [];
 
-  ngOnInit(): void {
+  constructor(
+    public restApi: RestApiService
+  ) { }
+
+  ngOnInit() {
+    this.loadEmployees()
   }
+
+  // Get employees list
+  loadEmployees() {
+    return this.restApi.getEmployees().subscribe((data: {}) => {
+      this.Employee = data;
+    })
+  }
+
+  // Delete employee
+  deleteEmployee(id) {
+    if (window.confirm('Are you sure, you want to delete?')){
+      this.restApi.deleteEmployee(id).subscribe(data => {
+        this.loadEmployees()
+      })
+    }
+  }  
 
 }
